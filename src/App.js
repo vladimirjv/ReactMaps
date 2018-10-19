4a2928e07491cc4b6fc3efad12400e74100eedfe
vladimirjv <ivladimirjuarez@outlook.com>
2018-10-18 22:13:31 -0500

    -

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
+/* jshint esversion: 6 */
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    /* jshint ignore:start */
+    // return (
+    //   <div className="App">
+    //     <p>
+    //       Hello from my react component
+    //     </p>
+    //   </div>
+    // );
+    /* jshint ignore:end */
+    // JSX
+    return React.createElement('div', {className:"App"}, 
+              React.createElement('p',null, 'Hello from my react component')
+            );
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+/* jshint esversion: 6 */
+import React, { Component } from 'react';
+
+export default class Person extends Component {
+  render() {
+    return (
+        /* jshint ignore:start */
+        <div>
+            
+        </div>
+        /* jshint ignore:end */
+    );
+  }
+}
