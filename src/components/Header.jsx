ed760a68436f01e2caee6d3bd140d6ab082e9a64
vladimirjv <ivladimirjuarez@outlook.com>
2018-10-25 21:14:26 -0500

    test method

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,17 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from "@material-ui/icons/Menu";
 
-function header(params) {
+class Header extends React.Component{
+  constructor(props) {
+    super(props);
+    this.onClickMethod = this.onClickMethod.bind(this);
+  }
+  onClickMethod(){
+    console.log('Hello');
+  }
+  render(){
     return(
-     /* jshint ignore: start */
+      /* jshint ignore: start */
       <AppBar
         color="primary"
         position="sticky"
@@ -16,11 +24,16 @@ function header(params) {
           <IconButton>
             <MenuIcon />
           </IconButton>
-          <h3>Header Maps</h3>
+          <h3
+            onClick={this.onClickMethod}
+          >
+            Header Maps
+          </h3>
         </Toolbar>
       </AppBar>
       /* jshint ignore: end */
     );
+  }
 }
 
-export default header;
+export default Header;
